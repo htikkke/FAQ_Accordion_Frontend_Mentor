ba3ee768d653dda33828b3feb501969386196c2c
htikkke <htikew574@gmail.com>
2026-05-06 16:50:33 +0630

    add js.

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-const plusIcons = document.querySelectorAll(".plus-icon");
-plusIcons.forEach((icon) => {
-  icon.addEventListener("click", () => {
-    
+const icons = document.querySelectorAll(".plus-icon");
+icons.forEach((icon) => {
+  icon.addEventListener("click", (event) => {
+    const parentDiv = event.target.closest(".questions-panel > div");
+    const answerDiv = parentDiv.querySelector(".answer");
+    if (answerDiv.style.display === "none" || answerDiv.style.display === "") {
+      answerDiv.style.display = "block";
+      icon.src = "images/icon-minus.svg";
+    } else {
+      answerDiv.style.display = "none";
+      icon.src = "images/icon-plus.svg";
+    }
   });
 });
